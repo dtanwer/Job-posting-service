bca2efe517871124894d0a666361b8788a9a3440
Deepak <tanwer644d@gmail.com>
2024-10-19 14:45:37 +0530

    Refactor sms-service.js to handle errors when sending SMS

--- services/sms-service.js
+++ services/sms-service.js
@@ -1,12 +1,20 @@
 const fast2sms = require('fast-two-sms')
 exports.sendSms = async (phoneNumber, message) => {
-    
-    await fast2sms.sendMessage({
-        authorization:process.env.SMS_API_KEY,
-        message,
-        numbers:[phoneNumber]
-    });
 
-    console.log("SMS send !");
+    try {
+
+        await fast2sms.sendMessage({
+            authorization:process.env.SMS_API_KEY,
+            message,
+            numbers:[phoneNumber]
+        });
+    
+        console.log("SMS send !");
+        
+    } catch (error) {
+        
+        console.log("Unable to send SMS!",error.message)
+    }
+    
 
 }
